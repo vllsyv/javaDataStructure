58e123290279dfacde2e2bcc9d080fe242b15f7a
XiaoQiang <1045507899@QQ.com>
2019-09-20 19:55:22 +0800

    leetcode--算法初级提升002：股票交易：0.1错误版

--- LeetCodeSolution/simple/_001towSum.java
+++ LeetCodeSolution/simple/_001towSum.java
@@ -21,7 +21,7 @@ import java.util.HashMap;
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  */
-public class towSum {
+public class _001towSum {
     public static void main(String[] args){
         int nums[] = {2, 7, 11, 15};
         int target = 9;
